fdff54677919848fc735747aee8968134a153a9b
Muawia Khan <amuawiakhan@gmail.com>
2019-01-11 17:50:50 +0100

    Separate some functionality from the main API code

--- pyphercises/app.py
+++ pyphercises/app.py
@@ -6,69 +6,89 @@ import datetime
 app = Flask(__name__)
 
 
+POSSIBLE_UNITS = ["metric", "imperial"]
+DEFAULT_CITY = "berlin"
+DEFAULT_COUNTRY = "de"
+DEFAULT_LONGITUDE = None
+DEFAULT_LATITUDE = None
+FORECAST_DELTA = 2
+
+def calculate_list_average(data):
+    return round(sum(data) / len(data), 2)
+
+def create_date_map(res):
+    DATE_MAP = {}
+    # Needed because the API returns 5 day forecast
+    start_forecast_date = datetime.datetime.now()
+    end_forecast_date = start_forecast_date + datetime.timedelta(days=FORECAST_DELTA)
+    interation_step = datetime.timedelta(days=1)
+    while start_forecast_date <= end_forecast_date:
+        start_forecast_date_fmt = start_forecast_date.strftime("%Y-%m-%d")      
+        DATE_MAP.setdefault(start_forecast_date_fmt, {})
+        for i in range(len(res["list"])):
+            if start_forecast_date_fmt in res["list"][i]["dt_txt"]:
+                DATE_MAP[start_forecast_date_fmt].setdefault("temperature", []).append(res["list"][i]["main"]["temp"])
+                DATE_MAP[start_forecast_date_fmt].setdefault("humidity", []).append(res["list"][i]["main"]["humidity"])
+                DATE_MAP[start_forecast_date_fmt].setdefault("wind", []).append(res["list"][i]["wind"]["speed"])
+                if "rain" in res["list"][i] and res["list"][i]["rain"]:
+                    DATE_MAP[start_forecast_date_fmt].setdefault("rain", []).append(res["list"][i]["rain"]["3h"])
+                if "snow" in res["list"][i] and res["list"][i]["snow"]:
+                    DATE_MAP[start_forecast_date_fmt].setdefault("snow", []).append(res["list"][i]["snow"]["3h"])
+        start_forecast_date += interation_step
+
+    return create_resp_dict(DATE_MAP, res)
+
+
+def create_resp_dict(dmap, res):
+    resp_dict = {}
+    # Calculating averages
+    for key, value in dmap.items():
+        resp_dict.setdefault(key, {})
+        resp_dict[key]["temperature"] = calculate_list_average(dmap[key]["temperature"])
+        resp_dict[key]["humidity"] = calculate_list_average(dmap[key]["humidity"])
+        resp_dict[key]["wind"] = calculate_list_average(dmap[key]["wind"])
+        if "rain" in dmap[key]:
+            resp_dict[key].setdefault("precipitation", {})
+            resp_dict[key]["precipitation"]["rain"] = calculate_list_average(dmap[key]["rain"])
+        if "snow" in dmap[key]:
+            resp_dict[key].setdefault("precipitation", {})
+            resp_dict[key]["precipitation"]["snow"] = calculate_list_average(dmap[key]["snow"])
+    resp_dict["city"] = res["city"]["name"]
+    resp_dict["country"] = res["city"]["country"]
+    resp_dict["coord"] = {}
+    resp_dict["coord"]["lat"] = res["city"]["coord"]["lat"]
+    resp_dict["coord"]["lon"] = res["city"]["coord"]["lon"]
+    return resp_dict
+
+
+
 @app.route('/')
 def root_endpoint():
     resp_dict = {
         "name": "Basic weather app",
         "description": "First cut weather app",
-        "version": version.__version__,
+        "weather_endpoint": "/weather", 
+        "version_endpoint": "/version",
     }
     return jsonify(resp_dict)
 
 
 @app.route('/weather')
 def weather_endpoint():
-    DATE_MAP = {}
-    POSSIBLE_UNITS = ["metric", "imperial"]
     APP_ID = "4055b8a5b1585d71d81af50461cd5257"
-    city_name = request.args.get('city', 'Berlin')
-    country_name = request.args.get('country', 'de')
-    longitude = request.args.get('lon', None)
-    latitude = request.args.get('lat', None)
+    city_name = request.args.get('city', DEFAULT_CITY)
+    country_name = request.args.get('country', DEFAULT_COUNTRY)
+    longitude = request.args.get('lon', DEFAULT_LONGITUDE)
+    latitude = request.args.get('lat', DEFAULT_LATITUDE)
     unit = request.args.get('units', 'metric')
-    r = requests.get('http://api.openweathermap.org/data/2.5/forecast?q={},{}&lat={}&lon={}&units={}&appid={}'.format(city_name, country_name, longitude, latitude, unit, APP_ID))
+    if unit not in POSSIBLE_UNITS:
+        return jsonify({'error': 'Invalid unit parameter'}, 400)
+    print('http://api.openweathermap.org/data/2.5/forecast?q={},{}&lat={}&lon={}&units={}&appid={}'.format(city_name, country_name, latitude, longitude, unit, APP_ID))
+    r = requests.get('http://api.openweathermap.org/data/2.5/forecast?q={},{}&lat={}&lon={}&units={}&appid={}'.format(city_name, country_name, latitude, longitude, unit, APP_ID))
+    if r.status_code != 200:
+        return jsonify(json.loads(r.text))
     forecast_resp = json.loads(r.content)
-
-    # Needed because the API returns 5 day forecast
-    start_forecast_date = datetime.datetime.now()
-    end_forecast_date = start_forecast_date + datetime.timedelta(days=2)
-    interation_step = datetime.timedelta(days=1)
-    while start_forecast_date <= end_forecast_date:
-        start_forecast_date_fmt = start_forecast_date.strftime("%Y-%m-%d")      
-        DATE_MAP.setdefault(start_forecast_date_fmt, {})
-        for i in range(len(forecast_resp["list"])):
-            if start_forecast_date_fmt in forecast_resp["list"][i]["dt_txt"]:
-                DATE_MAP[start_forecast_date_fmt].setdefault("temperature", []).append(forecast_resp["list"][i]["main"]["temp"])
-                DATE_MAP[start_forecast_date_fmt].setdefault("humidity", []).append(forecast_resp["list"][i]["main"]["humidity"])
-                DATE_MAP[start_forecast_date_fmt].setdefault("wind", []).append(forecast_resp["list"][i]["wind"]["speed"])
-                if forecast_resp["list"][i]["rain"]:
-                    DATE_MAP[start_forecast_date_fmt].setdefault("rain", []).append(forecast_resp["list"][i]["rain"]["3h"])
-                if forecast_resp["list"][i]["snow"]:
-                    DATE_MAP[start_forecast_date_fmt].setdefault("snow", []).append(forecast_resp["list"][i]["snow"]["3h"])
-        start_forecast_date += interation_step
-
-
-    resp_dict = {}
-    # Calculating averages
-    for key, value in DATE_MAP.items():
-        resp_dict.setdefault(key, {})
-        resp_dict[key]["temperature"] = round(sum(DATE_MAP[key]["temperature"]) / len(DATE_MAP[key]["temperature"]), 2)
-        resp_dict[key]["humidity"] = round(sum(DATE_MAP[key]["humidity"]) / len(DATE_MAP[key]["humidity"]), 2)
-        resp_dict[key]["wind"] = round(sum(DATE_MAP[key]["wind"]) / len(DATE_MAP[key]["wind"]), 2)
-        if "rain" in DATE_MAP[key]:
-            resp_dict[key].setdefault("precipitation", {})
-            resp_dict[key]["precipitation"]["rain"] = round(sum(DATE_MAP[key]["rain"]) / len(DATE_MAP[key]["rain"]), 2)
-        if "snow" in DATE_MAP[key]:
-            resp_dict[key].setdefault("precipitation", {})
-            resp_dict[key]["precipitation"]["snow"] = round(sum(DATE_MAP[key]["snow"]) / len(DATE_MAP[key]["snow"]), 2)
-
-    print(resp_dict)
-
-    resp_dict["city"] = forecast_resp["city"]["name"]
-    resp_dict["country"] = forecast_resp["city"]["country"]
-    resp_dict["coord"] = {}
-    resp_dict["coord"]["lat"] = forecast_resp["city"]["coord"]["lat"]
-    resp_dict["coord"]["lon"] = forecast_resp["city"]["coord"]["lon"]
+    resp_dict = create_date_map(forecast_resp)
     return jsonify(resp_dict)
 
 
